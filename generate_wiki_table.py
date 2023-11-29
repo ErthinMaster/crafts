ccb27fac2410c8227ac8420ff13f8e29a540b468
ErthinMaster <109388330+ErthinMaster@users.noreply.github.com>
2023-11-29 17:27:58 +0100

    Update and rename generate_wiki_table to generate_wiki_table.py

--- generate_wiki_table.py
+++ generate_wiki_table.py
@@ -10,9 +10,11 @@ def get_line(name, data, headers):
     return " || ".join(get_item(name, data, header, header_type) for header, header_type in headers.items())
 
 def generate_table_text(data, headers, title):
-    table_text = '{| class="darkTable" \n|+ ' + title + "\n"
+    table_text =  '{| class="darkTable" \n|+ ' + title + "\n"
     table_text += '!' + "!!".join(header.title() for header in headers.keys())+ '\n|-'
     table_text += '\n|-'.join(get_line(k, d, headers) for k, d in data.items())
+    table_text += '|}'
+    return table_text
 
 if __name__ == '__main__':
     potions = read_json("./potions.json")
@@ -23,4 +25,5 @@ if __name__ == '__main__':
         "ingrédients" : "dict",
         "descriptions" : "text"
     }
-    generate_table_text(potions, headers, "Liste des potions d'Alchimiste")
+    table_text = generate_table_text(potions, headers, "Liste des potions d'Alchimiste")
+    write_txt(table_text, "potions_wiki_table.txt")
