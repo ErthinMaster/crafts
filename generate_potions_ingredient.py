7c6dcd847262e1e7ab971715e3c93a3970de8428
ABe <axel.bentodasilva@microcameras.space>
2024-01-22 15:14:54 +0100

    Update

--- generate_potions_ingredient.py
+++ generate_potions_ingredient.py
@@ -1,16 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: Axel Bento da Silva
+# @Date:   2024-01-22 13:30:54
+# @Last Modified by:   Axel Bento da Silva
+# @Last Modified time: 2024-01-22 13:41:18
 import json, random
 from utils import load_json, write_json, load_txt
 
 
 def generate_ingredients_for_potions(potions, ingredients):
     for potion in potions.values():
-        nb_ingredients = random.randint(2, 2+potion['level'])
-        potion["ingredients"] = {val : random.randint(2, 2*potion['level']) for val in random.choices(ingredients, k=nb_ingredients)}
+        nb_ingredients = random.randint(2, 2+potion['niveau'])
+        potion["ingredients"] = {val : random.randint(2, 2*potion['niveau']) for val in random.choices(ingredients, k=nb_ingredients)}
 
 
 if __name__ == '__main__':
     potions = load_json("./potions.json")
-    ingredients = load_txt("./ingredients.txt")
+    ingredients = load_txt("./NewPlantes.txt")
     generate_ingredients_for_potions(potions, ingredients)
-    write_json(".potions_new.json", potions)
+    write_json("potions_new.json", potions)
     
--- utils.py
+++ utils.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: Axel Bento da Silva
+# @Date:   2024-01-22 13:30:54
+# @Last Modified by:   Axel Bento da Silva
+# @Last Modified time: 2024-01-22 13:41:53
+import json
 
 def load_json(path):
-    with open(path, "r") as f:
+    with open(path, "r", encoding='utf-8') as f:
         return json.load(f)
 
 def write_json(path, data):
-    with open(path, "w") as f:
+    with open(path, "w", encoding='utf-8') as f:
         json.dump(data, f, indent=4)
 
 def load_txt(path):
